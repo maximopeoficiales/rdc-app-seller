f704181f78cc77a3c2e9f658e98f43c00440d165
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-09 00:54:22 -0500

    ADD: funciones modificadas correctamente2

--- 006.sp_listar_detail_email_for_send.sql
+++ 006.sp_listar_detail_email_for_send.sql
@@ -1,5 +1,33 @@
 create function sp_listar_detail_email_for_send(ni_order_master_id integer)
-    returns TABLE(order_detail_id bigint, order_master_id bigint, product_type_id bigint, product_id character varying, quantity_products integer, quantity_products_return integer, monto numeric, monto_affected numeric, operation_type_id integer, reason_operation_id integer, product_url character varying, flag_offers integer, offers character varying, product character varying, classification_products_id bigint, model character varying, size character varying, image character varying, brand character varying, quantity_products_return_real integer, monto_affected_real numeric, flag_return integer, price_by_unit numeric, days_expiration integer, expiration_date date, seller_name character varying)
+    returns TABLE
+            (
+                order_detail_id               bigint,
+                order_master_id               bigint,
+                product_type_id               bigint,
+                product_id                    character varying,
+                quantity_products             integer,
+                quantity_products_return      integer,
+                monto                         numeric,
+                monto_affected                numeric,
+                operation_type_id             integer,
+                reason_operation_id           integer,
+                product_url                   character varying,
+                flag_offers                   integer,
+                offers                        character varying,
+                product                       character varying,
+                classification_products_id    bigint,
+                model                         character varying,
+                size                          character varying,
+                image                         character varying,
+                brand                         character varying,
+                quantity_products_return_real integer,
+                monto_affected_real           numeric,
+                flag_return                   integer,
+                price_by_unit                 numeric,
+                days_expiration               integer,
+                expiration_date               date,
+                seller_name                   character varying
+            )
     language plpgsql
 as
 $$
@@ -89,52 +117,65 @@ $$
 
 DECLARE
 
-    r order_detail%rowtype; 
-
-    d_purchase_date  date;
-
-    n_purchase_id    int8;
+    r               order_detail%rowtype;
+    d_purchase_date date;
+    n_purchase_id   int8;
 
 BEGIN
 
-	select a.purchase_date, a.purchase_id into d_purchase_date, n_purchase_id
+    select a.purchase_date, a.purchase_id
+    into d_purchase_date, n_purchase_id
 
-	from "order" a 
+    from "order" a
 
-	left join purchase b 
-
-	on b.purchase_id = a.purchase_id 
+             left join purchase b
+                       on b.purchase_id = a.purchase_id
 
     where a.order_id = ni_order_master_id;
 
 
+    RETURN QUERY
+        SELECT a.order_detail_id,
+               a.order_id as order_master_id,
+               a.product_type_id,
+               a.product_id,
+               a.quantity_products,
 
-   RETURN QUERY  
+               a.quantity_products_return,
+               a.monto,
+               a.monto_affected,
+               a.operation_type_id,
+               a.reason_operation_id,
 
-        SELECT a.order_detail_id, a.order_id, a.product_type_id, a.product_id, a.quantity_products, 
+               a.product_url,
+               a.flag_offers,
+               a.offers,
+               a.product,
+               a.classification_products_id,
+               a.model,
+               a.size,
+               a.image,
 
-         a.quantity_products_return, a.monto, a.monto_affected, a.operation_type_id, a.reason_operation_id, 
+               a.brand,
+               a.quantity_products_return_real,
+               a.monto_affected_real,
+               a.flag_return,
+               a.price_by_unit_total,
 
-         a.product_url, a.flag_offers, a.offers, a.product, a.classification_products_id, a.model, a.size, a.image, 
+               coalesce(pd.days_expiration, 60) as                days_expiration,
+               d_purchase_date + coalesce(pd.days_expiration, 60) expiration_date,
 
-         a.brand, a.quantity_products_return_real, a.monto_affected_real, a.flag_return, a.price_by_unit_total,
-
-         coalesce(pd.days_expiration, 60) as days_expiration, d_purchase_date + coalesce(pd.days_expiration, 60) expiration_date,
-
-         a.seller_name 
+               a.seller_name
 
         FROM order_detail a
 
-        LEFT JOIN purchase_detail pd 
+                 LEFT JOIN purchase_detail pd
+                           ON pd.product_id = a.product_id and
+                              pd.purchase_id = n_purchase_id and
+                              pd.suborder = pd.suborder
 
-        ON pd.product_id = a.product_id and 
-
-           pd.purchase_id  = n_purchase_id and
-           pd.suborder = pd.suborder 
-
-        WHERE a.order_id = ni_order_master_id AND
-
-		a.quantity_products_return > 0;
+        WHERE a.order_id = ni_order_master_id
+          AND a.quantity_products_return > 0;
 
 END;
 

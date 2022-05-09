1e19ab89f0632ff5b9432fce6fa46a39672a28f0
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-09 00:47:34 -0500

    ADD: funciones modificadas correctamente

--- 011.sp_listar_order_details_x_order.sql
+++ 011.sp_listar_order_details_x_order.sql
@@ -1,14 +1,49 @@
-CREATE FUNCTION public.sp_listar_order_details_x_order(ni_order_id integer) RETURNS SETOF public."order_detail"
+CREATE FUNCTION public.sp_listar_order_details_x_order(ni_order_id integer)
+    RETURNS TABLE
+            (
+                order_detail_id               bigint,
+                order_id                      bigint,
+                product_type_id               bigint,
+                product_id                    character varying,
+                quantity_products             integer,
+                quantity_products_return      integer,
+                monto                         numeric,
+                monto_affected                numeric,
+                operation_type_id             integer,
+                reason_operation_id           integer,
+                product_url                   character varying,
+                flag_offers                   integer,
+                offers                        character varying,
+                product                       character varying,
+                classification_products_id    bigint,
+                model                         character varying,
+                size                          character varying,
+                image                         character varying,
+                brand                         character varying,
+                quantity_products_return_real integer,
+                monto_affected_real           numeric,
+                flag_return                   integer,
+                price_by_unit                 numeric,
+                days_expiration               integer,
+                expiration_date               date,
+                price_by_unit_total           numeric,
+                promotion_code                character varying,
+                promotion                     character varying,
+                cupon_number                  character varying,
+                promotion_discount_amount     numeric,
+                itemn_number                  integer,
+                color                         character varying,
+                suborder                      character varying,
+                type_product                  character varying,
+                seller_name                   character varying,
+                seller_id                     character varying,
+                order_master_id               bigint
+            )
     LANGUAGE plpgsql
-    AS $$
-DECLARE
-    r order_detail%rowtype;
+AS
+$$
 BEGIN
-    FOR r IN
-        SELECT * FROM order_detail WHERE order_id = ni_order_id
-    LOOP
-        RETURN NEXT r;
-    END LOOP;
-    RETURN;
+    RETURN QUERY
+        SELECT c.*, c.order_id as order_master_id FROM order_detail as c WHERE c.order_id = ni_order_id;
 END;
 $$;
--- 012.sp_listar_order_x_tikect.sql
+++ 012.sp_listar_order_x_tikect.sql
@@ -1,21 +1,57 @@
-CREATE FUNCTION public.sp_listar_order_x_tikect(vi_tikect character varying) RETURNS SETOF public."order"
-    LANGUAGE plpgsql
-AS
+CREATE FUNCTION public.sp_listar_order_x_tikect(vi_tikect character varying)
+    RETURNS TABLE
+            (
+                order_id                 bigint,
+                prospect_order           bigint,
+                order_number             varchar,
+                identity_document        varchar,
+                email                    varchar,
+                purchase_type_id         integer,
+                purchase_date            date,
+                number_products          integer,
+                number_products_unique   integer,
+                number_products_return   integer,
+                number_products_change   integer,
+                amount_total             numeric,
+                amount_total_return      numeric,
+                amount_total_change      numeric,
+                estado_request           integer,
+                flag_send_email          integer,
+                id_qr                    varchar,
+                created_at               timestamp with time zone,
+                date_id                  date,
+                coordinates              varchar,
+                category_id              integer,
+                type_return_id           integer,
+                return_method_id         integer,
+                bar_code                 varchar,
+                ip_address               varchar,
+                purchase_id              bigint,
+                email_option             varchar,
+                state_email_option       integer,
+                person_return            integer,
+                person_first_name        varchar,
+                person_last_name         varchar,
+                person_identity_document varchar,
+                flag_send_email_security integer,
+                type_order               varchar,
+                phone                    varchar,
+                order_master_id          bigint,
+                monto_total              numeric,
+                monto_total_return       numeric,
+                monto_total_change       numeric
+            )
+as
 $$
-DECLARE
-    r order%rowtype;
 BEGIN
-    FOR r IN
+    RETURN QUERY
         SELECT *,
-               order_id            as order_master_id,
-               amount_total        as monto_total,
-               amount_total_return as monto_total_return,
-               amount_total_change as monto_total_change
-        FROM public."order"
-        WHERE order_number = vi_tikect
-        LOOP
-            RETURN NEXT r;
-        END LOOP;
-    RETURN;
+               c.order_id            as order_master_id,
+               c.amount_total        as monto_total,
+               c.amount_total_return as monto_total_return,
+               c.amount_total_change as monto_total_change
+        FROM public."order" as c
+        WHERE c.order_number = vi_tikect;
 END;
-$$;
+$$
+    LANGUAGE plpgsql;

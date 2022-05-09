d8db47ff6bb71337fdd213938a85c6b55e19e069
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-09 01:09:51 -0500

    ADD: funciones modificadas correctamente2

--- 000.drop_functions.sql
+++ 000.drop_functions.sql
@@ -1,5 +1,23 @@
+-- deprecada
 drop function if exists fnt_get_git_card_white_list;
+-- funciones a modificar
+drop function if exists sp_list_order_internet;
+drop function if exists sp_list_order_store;
+drop function if exists sp_list_products_by_order;
+drop function if exists sp_listar_detail_email_for_send;
+drop function if exists sp_listar_email_for_send;
+drop function if exists sp_listar_email_for_send_faild;
+drop function if exists sp_listar_email_for_send_security;
+drop function if exists sp_listar_order_detail_x_tikect;
 drop function if exists sp_listar_order_details_x_order;
 drop function if exists sp_listar_order_x_tikect;
+drop function if exists sp_migra_order_detail_msa;
+drop function if exists sp_migra_order_msa;
+drop function if exists sp_save_order;
+drop function if exists sp_save_send_email;
+drop function if exists sp_save_send_email_security;
+drop function if exists sp_save_state_done_msa;
+drop function if exists sp_save_tikect_done_msa;
+drop function if exists sp_update_qr;
 
 -- sp_save_send_email_purchase funcion rota

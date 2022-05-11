35f00f462acb1ff00084c9d2b6c87ef0dc3c0a4c
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-11 11:35:38 -0500

    ADD: funciones modificads

--- 002.alter_order_detail.sql
+++ 002.alter_order_detail.sql
@@ -6,3 +6,6 @@ alter table public.order_detail add column reason_text varchar;
 
 alter table public.order_detail add column id_seller bigint;
 ALTER TABLE public.order_detail ADD CONSTRAINT order_detail_fk FOREIGN KEY (id_seller) REFERENCES public.seller(id);
+
+comment on column public.order_detail.reason_text is 'razon de la operacion';
+comment on column public.order_detail.id_seller is 'identificador de la tabla seller';
